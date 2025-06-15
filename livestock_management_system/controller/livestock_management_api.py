b954505acc4982b92a1df3ef2c4e336ad63432b6
AnthonyG <AnthonyG@ICT-16>
2025-06-15 14:55:03 +0600

    asset vaccine schedule api

--- livestock_management_system/controller/livestock_management_api.py
+++ livestock_management_system/controller/livestock_management_api.py
@@ -1,6 +1,6 @@
 import json
 from django.http import JsonResponse
-from livestock_management_system.helper.livestock_management_helper_class import HealthRecordRequest, add_asset_vaccination_schedule, get_assets_list, add_assets_health_record,get_assets_health_record, get_asset_vaccine_list
+from livestock_management_system.helper.livestock_management_helper_class import HealthRecordRequest, add_asset_vaccination_schedule, get_asset_vaccination_schedule, get_assets_list, add_assets_health_record,get_assets_health_record, get_asset_vaccine_list
 from django.views.decorators.http import require_GET
 from pydantic import ValidationError
 
@@ -84,4 +84,20 @@ def create_asset_vaccination_schedule(request):
         result = add_asset_vaccination_schedule(record)  # validation happens here
         return JsonResponse(result)
      except ValidationError as e:
-         return JsonResponse({"status": "failed", "errors": e.errors()}, status=400)        
+         return JsonResponse({"status": "failed", "errors": e.errors()}, status=400)
+
+'''
+ # @ Author: Tanmay Anthony Gomes
+ # @ Create Time: 2025-06-11 15:00:13
+ # @ Modified by: Tanmay Anthony Gomes
+ # @ Modified time: 2025-06-15 11:04:31
+ # @ Description: This api will get the schedule of vaccine
+ '''
+def get_vaccination_schedule(request):
+     try:
+        data = json.loads(request.body)
+        record = VaccinationScheduleRequest(**data)  # validation happens here        
+        result = get_asset_vaccination_schedule(record)  # validation happens here
+        return JsonResponse(result)
+     except ValidationError as e:
+         return JsonResponse({"status": "failed", "errors": e.errors()}, status=400)                
--- livestock_management_system/helper/livestock_management_helper_class.py
+++ livestock_management_system/helper/livestock_management_helper_class.py
@@ -115,4 +115,23 @@ def add_asset_vaccination_schedule(record: VaccinationScheduleRequest):
             return _response(result["status"], result["message"], data)
 
     except Exception as ex:
-        return _response("error", str(ex))    
+        return _response("error", str(ex))
+
+
+def get_asset_vaccination_schedule(record: VaccinationScheduleRequest):
+    try:
+        with get_db_connection() as conn: # calling get_db_connection for getting the connection string
+            rows = call_db_function(conn, "public.fn_get_asset_vaccination_schedule", [record.json()]) # calling fn_get_assets_list function from DB  to get data.
+
+            if not rows:
+                return _response("failed", "Error Occured While Processing Request")
+
+            result = rows[0]  
+            data = result["data"]
+            if isinstance(data, str):
+                data = json.loads(data)
+
+            return _response(result["status"], result["message"], data)
+
+    except Exception as ex:
+        return _response("error", str(ex))   
