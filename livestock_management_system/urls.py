0365436e731ecf37ec48de5eb829c81028cd9ee5
AnthonyG <AnthonyG@ICT-16>
2025-06-28 15:10:19 +0600

    removed old URLS from routing

--- livestock_management_system/urls.py
+++ livestock_management_system/urls.py
@@ -9,14 +9,14 @@ from livestock_management_system.controller.vaccination_schedule_service_api imp
 from livestock_management_system.controller.vaccine_service_api import VaccineServiceAPIView
 
 urlpatterns = [
-    path('get-assets/', livestock_management_api.fetch_assets, name='fetch_assets'),
-    path('add-health-record/', livestock_management_api.create_health_record, name='create_health_record'),
-    path('get-health-record/', livestock_management_api.get_health_record, name='get_health_record'),
-    path('get-vaccine-list/', livestock_management_api.get_vaccine_list, name='get_vaccine_list'),
-    path('add-asset-vaccination-schedule/', livestock_management_api.create_asset_vaccination_schedule, name='create_asset_vaccination_schedule'),    
-    path('get-vaccination-schedule/', livestock_management_api.get_vaccination_schedule, name='get_vaccination_schedule'),  
-    path('get-medical-condition/', livestock_management_api.get_medical_condition, name='get_medical_condition'),     
-    path('get-medical-condition-severity/', livestock_management_api.get_medical_condition_severity, name='get_medical_condition_severity'),
+    # path('get-assets/', livestock_management_api.fetch_assets, name='fetch_assets'),
+    # path('add-health-record/', livestock_management_api.create_health_record, name='create_health_record'),
+    # path('get-health-record/', livestock_management_api.get_health_record, name='get_health_record'),
+    # path('get-vaccine-list/', livestock_management_api.get_vaccine_list, name='get_vaccine_list'),
+    # path('add-asset-vaccination-schedule/', livestock_management_api.create_asset_vaccination_schedule, name='create_asset_vaccination_schedule'),    
+    # path('get-vaccination-schedule/', livestock_management_api.get_vaccination_schedule, name='get_vaccination_schedule'),  
+    # path('get-medical-condition/', livestock_management_api.get_medical_condition, name='get_medical_condition'),     
+    # path('get-medical-condition-severity/', livestock_management_api.get_medical_condition_severity, name='get_medical_condition_severity'),
     path('assets-service/', AssetsServiceAPIView.as_view(), name='assets-service'),
     path('health-record-service/', HealthRecordServiceAPIView.as_view(), name='health-record-service'), 
     path('medical-condition-service/', MedicalConditionServiceAPIView.as_view(), name='medical-condition-service'),
