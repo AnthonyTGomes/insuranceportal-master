830811b8cec04e42485c26dff134e564522219f4
AnthonyG <AnthonyG@ICT-16>
2025-07-13 16:55:11 +0600

    bug fix

--- common/common_class/util.py
+++ common/common_class/util.py
@@ -17,9 +17,10 @@ def _response(status, message, data=None):
  # @ Create Time: 2025-06-12 15:13:41
  # @ Modified by: Tanmay Anthony Gomes
  # @ Modified time: 2025-06-12 17:49:03
- # @ Description: function for getting user id from the request
+ # @ Description: function for getting user id from the request and injecting that into by_user_id property
  '''
 def build_request_with_user(model_class, request, method='GET'):
-    data = request.query_params if method == 'GET' else request.data
+    data = request.query_params.dict() if method == 'GET' else request.data
+    print(data)
     payload = {**data, "by_user_id": request.user.id}
     return model_class(**payload)
