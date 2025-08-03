0008da3c2a85eaa9d29b9939f57eb583bcb80b54
AnthonyG <AnthonyG@ICT-16>
2025-08-03 15:55:47 +0600

    ok

--- insurance_management_system/urls.py
+++ insurance_management_system/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from insurance_management_system.controller.insurance_application_service_api import InsuranceApplicationServiceAPIView
 from insurance_management_system.controller.insurance_product_service_api import InsuranceProductServiceAPIView
-from insurance_management_system.controller.farmer_service_api import FarmerServiceAPIView, InsuranceServiceAPIView
+from insurance_management_system.controller.farmer_service_api import FarmerServiceAPIView
 urlpatterns = [
     path('insurance-application-service/', InsuranceApplicationServiceAPIView.as_view(), name='insurance-application-service'),
     path('insurance-product-service/', InsuranceProductServiceAPIView.as_view(), name='insurance-product-service'), 
