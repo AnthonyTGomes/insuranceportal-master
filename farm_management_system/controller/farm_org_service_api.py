e407ff2af504ed556d9bbb368a55bfa27b5c3c75
AnthonyG <AnthonyG@ICT-16>
2025-09-02 11:21:58 +0600

    get api of farm org is added

--- farm_management_system/controller/farm_org_service_api.py
+++ farm_management_system/controller/farm_org_service_api.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from common.common_class.util import build_request_with_user
-from farm_management_system.helper.insurance_management_helper_class import insert_farm_organizations, update_farm_organizations
+from farm_management_system.helper.insurance_management_helper_class import get_farm_organization_list, insert_farm_organizations, update_farm_organizations
 from farm_management_system.helper.model_class import FarmOrgInfoRequest
 
 class FarmOrgServiceAPIView(APIView):
@@ -46,3 +46,20 @@ class FarmOrgServiceAPIView(APIView):
             return JsonResponse(result)
         except ValidationError as e:
             return JsonResponse({"status": "failed", "errors": e.errors()}, status=400)   
+        
+    """
+    @ Author: Tanmay Anthony Gomes
+    @ Create Time: 01-09-2025
+    @ Modified by: 
+    @ Modified time: 
+    @ Description: API to get farm organizations
+    """
+
+    def get(self, request):
+        try:
+            record = build_request_with_user(FarmOrgInfoRequest, request, method='GET')
+            #record = AssetInfoRequest(**request.data)
+            result = get_farm_organization_list(record)
+            return JsonResponse(result)
+        except ValidationError as e:
+            return JsonResponse({"status": "failed", "errors": e.errors()}, status=400)          
--- farm_management_system/helper/insurance_management_helper_class.py
+++ farm_management_system/helper/insurance_management_helper_class.py
@@ -54,3 +54,29 @@ def update_farm_organizations(record: FarmOrgInfoRequest):
 
     except Exception as ex:
         return _response("error", str(ex))    
+    
+
+'''
+ # @ Author: Tanmay Anthony Gomes
+ # @ Create Time: 
+ # @ Modified by: Tanmay Anthony Gomes
+ # @ Modified time: 
+ # @ Description: Function for getting farm organizartions List
+'''
+def get_farm_organization_list(record: FarmOrgInfoRequest):
+    try:
+        with get_db_connection() as conn: # calling get_db_connection for getting the connection string
+            rows = call_db_function(conn, "public.fn_get_farm_organization_list", [record.json()]) # calling fn_get_assets_list function from DB  to get data.
+
+            if not rows:
+                return _response("failed", "Error Occured While Processing Request")
+
+            result = rows[0]  
+            data = result["data"]
+            if isinstance(data, str):
+                data = json.loads(data)
+
+            return _response(result["status"], result["message"], data)
+
+    except Exception as ex:
+        return _response("error", str(ex))       
