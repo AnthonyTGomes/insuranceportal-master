1c903cb2bdad69ea622aa8f0f24a69ed3ccf8f66
AnthonyG <AnthonyG@ICT-16>
2025-09-18 17:20:40 +0600

    ok

--- livestock_management_system/controller/production_type_service_api.py
+++ livestock_management_system/controller/production_type_service_api.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from common.common_class.util import build_request_with_user
-from livestock_management_system.helper.livestock_management_helper_class import get_asset_production_records
+from livestock_management_system.helper.livestock_management_helper_class import get_asset_production_type
 from livestock_management_system.helper.model_class import AssetProductionTypeRequest
 
 class ProductionTypeServiceAPIView(APIView):
@@ -25,7 +25,7 @@ class ProductionTypeServiceAPIView(APIView):
         try:
             record = build_request_with_user(AssetProductionTypeRequest, request, method='GET')
             #record = VaccineRequest(**request.data)  # validation happens here        
-            result = get_asset_production_records(record)  # validation happens here
+            result = get_asset_production_type(record)  # validation happens here
             return JsonResponse(result)
         except ValidationError as e:
             return JsonResponse({"status": "failed", "errors": e.errors()}, status=400)         
